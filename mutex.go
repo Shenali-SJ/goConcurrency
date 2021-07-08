f082aef362a90e64f90a2490420e1d31e3e8db71
Shenali-SJ <shenalijayakody@gmail.com>
2021-07-08 20:54:03 +0530

    complete mutex

--- mutex.go
+++ mutex.go
@@ -6,6 +6,7 @@ import (
 )
 
 var x = 0
+var y = 0
 
 func increment(wg *sync.WaitGroup, m *sync.Mutex) {
 	m.Lock()
@@ -14,7 +15,15 @@ func increment(wg *sync.WaitGroup, m *sync.Mutex) {
 	wg.Done()
 }
 
+func increment2(wg *sync.WaitGroup, channel chan bool) {
+	channel <- true
+	y = y + 1
+	<- channel
+	wg.Done()
+}
+
 func main() {
+	//mutex for race condition
 	var wGroup sync.WaitGroup
 	var mutex sync.Mutex
 	for i := 0; i < 1000; i++ {
@@ -23,4 +32,16 @@ func main() {
 	}
 	wGroup.Wait()
 	fmt.Println("Final value of x ", x)
+
+	fmt.Println()
+
+	//channels for race condition
+	var wGroup2 sync.WaitGroup
+	channel := make(chan bool, 1)
+	for i := 0; i < 1000; i++ {
+		wGroup2.Add(1)
+		go increment2(&wGroup2, channel)
+	}
+	wGroup2.Wait()
+	fmt.Println("Final value of y ", x)
 }
